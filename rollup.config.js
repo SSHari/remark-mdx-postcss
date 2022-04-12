37401a64168cc3e228b47db61af2143fab5a3c67
SSHari <thesshguy@gmail.com>
2022-04-12 12:59:36 -0400

    fix: 🐛 build process to use typescript directly

--- rollup.config.js
+++
@@ -1,6 +0,0 @@
-import { createFileConfig, createExternalDeps } from 'ssh-dev-scripts/src/rollup.config';
-export default createFileConfig({
-  external: [...createExternalDeps({ '@babel/runtime': 'version' }), 'postcss'],
-  input: 'src/index.ts',
-  output: [{ file: 'dist/index.js', format: 'esm' }],
-});
